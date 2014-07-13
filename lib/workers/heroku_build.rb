ff373dde893291a2efda8cc2741973790b06e1b5
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2014-07-13 14:28:59 +0200

    Use app_name in redis to deploy to

--- lib/workers/heroku_build.rb
+++ lib/workers/heroku_build.rb
@@ -9,19 +9,25 @@ module Workers
       @url = url
       @version = version
       @request_id = options['request_id']
-      # TODO Use a selected app
-      @app_name = 'cryptic-atoll-7822'
 
       begin
         setup_clients
+        fetch_app
         submit_build
       rescue Errors::AuthenticationMissing
         log(auth_missing: true)
+      rescue Errors::AppMissing
+        log(app_missing: true)
       end
     end
 
     def setup_clients
       @heroku = Services::Heroku.connect_redis(@user_id)
+    end
+
+    def fetch_app
+      @app_name = $redis.hget(heroku_key, 'app_name')
+      raise Errors::AppMissing unless @app_name
     end
 
     def submit_build
@@ -39,8 +45,12 @@ module Workers
 
     private
 
+    def heroku_key
+      "heroku_#{@user_id}"
+    end
+
     def log(data={}, &blk)
-      Pliny.log({ heroku_builder: true, user: @user_id, request_id: @request_id }.merge(data), &blk)
+      Pliny.log({ heroku_builder: true, user: @user_id, request_id: @request_id, app_name: @app_name }.merge(data), &blk)
     end
   end
 end
